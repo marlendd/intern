925f690921fb1c1c44176208feefd37e31939d37
marlendd <107889061+marlendd@users.noreply.github.com>
2025-01-17 21:00:35 +0300

    Update SocketClient.h

--- Program1/include/SocketClient.h
+++ Program1/include/SocketClient.h
@@ -7,13 +7,14 @@
 class SocketClient {
 public:
     SocketClient(const std::string& host, int port);
+    ~SocketClient();
     bool connectToServer();
     void sendData(const std::map<char, int>& data);
 
 private:
     std::string serverHost;
     int serverPort;
-    int serverSocket;
+    int clientSocket;
 };
 
 #endif // SOCKETCLIENT_H
