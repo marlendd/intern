5eb2ed93c959863bb9a67145caaba08174e94afd
marlendd <107889061+marlendd@users.noreply.github.com>
2025-01-17 18:55:50 +0300

    Update Buffer.h

--- Program1/include/Buffer.h
+++ Program1/include/Buffer.h
@@ -1,1 +1,20 @@
-//#
+#ifndef BUFFER_H
+#define BUFFER_H
+
+#include <map>
+#include <mutex>
+
+class Buffer {
+public:
+    Buffer() = default;
+
+    void write(const std::map<char, int>& data);
+    std::map<char, int> read();
+    bool hasData() const;
+
+private:
+    std::map<char, int> buffer;
+    mutable std::mutex mutex;
+};
+
+#endif // BUFFER_H
