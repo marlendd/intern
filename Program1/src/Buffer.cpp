e3e15ffdcde053b5a085a4d41bb8bf33631ecf6d
marlendd <107889061+marlendd@users.noreply.github.com>
2025-01-17 18:54:09 +0300

    Update Buffer.cpp

--- Program1/src/Buffer.cpp
+++ Program1/src/Buffer.cpp
@@ -1,1 +1,16 @@
-//plchldr
+#include "Buffer.h"
+
+void Buffer::write(const std::map<char, int>& data) {
+    std::lock_guard<std::mutex> lock(mutex);
+    buffer = data;
+}
+
+std::map<char, int> Buffer::read() {
+    std::lock_guard<std::mutex> lock(mutex);
+    return buffer;
+}
+
+bool Buffer::hasData() const {
+    std::lock_guard<std::mutex> lock(mutex);
+    return !buffer.empty();
+}
