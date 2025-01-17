b0a551beaa67183c7f0f51b6fd4689f5c63982bd
marlendd <107889061+marlendd@users.noreply.github.com>
2025-01-17 18:59:11 +0300

    Update SocketServer.h

--- Program2/include/SocketServer.h
+++ Program2/include/SocketServer.h
@@ -1,1 +1,15 @@
+#ifndef SOCKETSERVER_H
+#define SOCKETSERVER_H
 
+#include <string>
+
+class SocketServer {
+public:
+    explicit SocketServer(int port);
+    void run();
+
+private:
+    int serverPort;
+};
+
+#endif // SOCKETSERVER_H
