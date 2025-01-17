abd6077eeb78b4807bed443970f9ed527c3e6eb7
marlendd <107889061+marlendd@users.noreply.github.com>
2025-01-17 18:55:29 +0300

    Update ThreadManager.h

--- Program1/include/ThreadManager.h
+++ Program1/include/ThreadManager.h
@@ -1,1 +1,24 @@
-//#
+#ifndef THREADMANAGER_H
+#define THREADMANAGER_H
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <map>
+
+class ThreadManager {
+public:
+    void run();
+
+private:
+    void handleInput();
+    void processData();
+
+    std::map<char, int> buffer;
+    std::mutex bufferMutex;
+    std::condition_variable bufferCondition;
+    bool hasData = false;
+};
+
+#endif // THREADMANAGER_H
